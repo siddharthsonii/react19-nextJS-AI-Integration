10afccaecfedd2dfa0916539b4905d313d0f22d3
Siddharth Soni <siddharth.soni588@gmail.com>
2026-02-15 21:05:00 +0530

    React Started with Designing

---
+++ 01_start/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hey, I am learning React v{React.version}</h1>
+      <p>maza aa rha hai mujhe</p>
+    </>
+  );
+}
+
+export default App;
